8b017a29f3c05bc947ffecadea85112c2ed711f4
Mohamad Khaseeb <59807138+m7amd@users.noreply.github.com>
2021-07-19 16:53:28 +0300

    updating

--- Js/main.js
+++ Js/main.js
@@ -62,43 +62,6 @@ footerLinks.forEach((item) => {
 
 });
 
-// Form Validate
-
-let form = document.querySelector('form'),
-statusTxt = form.querySelector(".send-btn span");
-
-form.onsubmit = (e) => {
-
-  e.preventDefault();
-  statusTxt.style.color = "#FF3946";
-  statusTxt.style.display = "inline-block";
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "./php/form.php", true);
-  xhr.onload = () => {
-
-    if (xhr.readyState === 4 && xhr.status == 200) {
-
-      let response = xhr.response;
-      if (response.indexOf("Email Field, is required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")) {
-
-        statusTxt.style.color = "red";
-
-      } else {
-
-        form.reset();
-        setTimeout(() => {
-          statusTxt.style.display = "none";
-        }, 3000) 
-      }
-      statusTxt.innerText = response;
-    }
-
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
-};
-
 // INITIALIZE AOS
 AOS.init();
 
@@ -117,9 +80,5 @@ $( () => {
   // });
     // Toggle Humbrger menu
     // $('nav.show').css('opacity','0')
-    $('#toggle').on('click', function(){
-        $(this).toggleClass('show');
-      return false;
-    });
 });
 
