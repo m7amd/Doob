484a21ad4c84fe486fe5e15c2b76616959aca30d
Mohamad Khaseeb <59807138+m7amd@users.noreply.github.com>
2021-07-19 16:42:41 +0300

    edit path of js files

--- Js/main.js
+++ Js/main.js
@@ -24,34 +24,102 @@ scrollToTop.onclick = function () {
 };
 
 
-// jQuery
+// Scroll To Element 
 
-$(document).ready(function () {
+const headerLinks = document.querySelectorAll(".header nav a");
 
-  // smooth scroll 
+headerLinks.forEach((item) => {
 
-  $('.header nav a').on('click', function (e) {
+  item.addEventListener('click', (e) => {
 
     e.preventDefault();
+    
+    let ele = document.getElementById(item.getAttribute("data-scroll"));
 
-    $('html, body').animate({
-
-      scrollTop: $('#' + $(this).data('scroll')).offset().top + 1
-
-    }, 1100);
+    ele.scrollIntoView({
+      behavior : "smooth",
+    });
 
   });
 
-  $('.footer .links a').on('click', function (e) {
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-      scrollTop: $('#' + $(this).data('scroll')).offset().top + 1
-
-    }, 1100);
-
-  });
-  
 });
+
+const footerLinks = document.querySelectorAll(".footer .links a");
+
+footerLinks.forEach((item) => {
+
+  item.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    
+    let ele = document.getElementById(item.getAttribute("data-scroll"));
+
+    ele.scrollIntoView({
+      behavior : "smooth",
+    });
+
+  });
+
+});
+
+// Form Validate
+
+let form = document.querySelector('form'),
+statusTxt = form.querySelector(".send-btn span");
+
+form.onsubmit = (e) => {
+
+  e.preventDefault();
+  statusTxt.style.color = "#FF3946";
+  statusTxt.style.display = "inline-block";
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "./php/form.php", true);
+  xhr.onload = () => {
+
+    if (xhr.readyState === 4 && xhr.status == 200) {
+
+      let response = xhr.response;
+      if (response.indexOf("Email Field, is required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")) {
+
+        statusTxt.style.color = "red";
+
+      } else {
+
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000) 
+      }
+      statusTxt.innerText = response;
+    }
+
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+};
+
+// INITIALIZE AOS
+AOS.init();
+
+// INITIALIZE niceScroll
+
+$( () => {
+  // $("body").niceScroll({
+  //   touchbhavior: true,
+  //   zindex: '999999',
+  //   scrollspeed: '0', // 30
+  //   cursorcolor:"#314584",
+  //   background:"#ecf0f1",
+  //   cursorborder:"1px solid #314584",
+  //   cursorborderradius: '4px',
+  //   cursorwidth:"8px",
+  // });
+    // Toggle Humbrger menu
+    // $('nav.show').css('opacity','0')
+    $('#toggle').on('click', function(){
+        $(this).toggleClass('show');
+      return false;
+    });
+});
+
